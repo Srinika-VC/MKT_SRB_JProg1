3bba30d4c9ac3abe23704ca1c62780265a6f047c
Srinika <srinikavc2791@gmail.com>
2024-11-16 18:49:11 +0530

    class 28 all programs

---
+++ Class_28_Set_interface.java
@@ -0,0 +1,14 @@
+package cLass_28_allprograms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Class_28_Set_interface 
+{
+public static void main(String[] args)
+{
+	Set s1= new HashSet();
+	
+}
+
+}
---
+++ Set_interface_2.java
@@ -0,0 +1,10 @@
+package cLass_28_allprograms;
+
+public class Set_interface_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
