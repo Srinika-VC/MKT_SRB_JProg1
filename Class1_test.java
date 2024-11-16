517384882652d969b83d71baf9e4f10ef928b19d
Srinika <srinikavc2791@gmail.com>
2024-11-16 19:12:07 +0530

    class1 and class 2 from package 1 and 2 have a connection

---
+++ Class1_test.java
@@ -0,0 +1,33 @@
+package package1;
+
+public class Class1_test 
+{
+	public void add()
+	{
+		
+	}
+
+	 void subtract()
+	{
+		
+	}
+	
+	protected void multi()
+	{
+		
+	}
+	
+	private void divid()
+	{
+		
+	}
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
---
+++ Class2_test.java
@@ -0,0 +1,15 @@
+package package2;
+
+import package1.Class1_test;
+
+public class Class2_test {
+
+	public static void main(String[] args)
+	{
+		Class1_test c1	= new Class1_test();
+		c1.add();
+		
+
+	}
+
+}
