173cc5acee1f778f1a2d35db30f721c169ead1db
Srinika <srinikavc2791@gmail.com>
2024-11-16 19:18:41 +0530

    abstractclass_Case2

---
+++ Test_case1.java
@@ -0,0 +1,16 @@
+package abstractclass_Case2;
+
+public class Test_case1 
+{
+
+	public static void main(String[] args) 
+	{
+		Test_case2 t1= new Test_case2();
+		t1.add();
+		t1.div();
+		t1.multiple();
+		
+
+	}
+
+}
---
+++ Test_case2.java
@@ -0,0 +1,22 @@
+package abstractclass_Case2;
+
+public class Test_case2 
+{
+	 public void add()
+	 {
+		 
+	 }
+    private void subtract() // its private because we cannot use this in Test_case1
+	 {
+     }
+	 void multiple()
+	 {
+     }
+	 protected void div()
+	 {
+
+	 }	
+
+	}
+
+
